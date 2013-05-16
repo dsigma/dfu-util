eae0079fb6ddf9efd1186fb16e7000b9e6583259
Tormod Volden <debian.tormod@gmail.com>
2013-05-16 21:34:46 +0200

    Adjust file create mode to be more generous
    Leave it to umask to restrict it further.
    
    Fix up of commmit 7d55101d383f78b7d063ff22bb573977abe0b4e2
    
    Signed-off-by: Tormod Volden <debian.tormod@gmail.com>

--- src/main.c
+++ src/main.c
@@ -570,7 +570,7 @@ status_again:
 	switch (mode) {
 	case MODE_UPLOAD:
 		/* open for "exclusive" writing */
-		fd = open(file.name, O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, 0600);
+		fd = open(file.name, O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, 0666);
 		if (fd < 0)
 			err(EX_IOERR, "Cannot open file %s for writing", file.name);
 
